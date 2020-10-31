e6cfeb24dfa61563e10cec1e64d3e2a7f408bca9
alexkobrin <storyday2012@gmail.com>
2020-10-31 20:24:32 +0100

    work on search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,27 +2,33 @@ import React from 'react';
 import './search.scss';
 function search(props) {
 
-   const  handleChangle = event => {
-    event.preventDefault()
-     //  let result = event.target.select    
+    const handleChangle = event => {
+
+        if(event !== undefined) {
+                     let querry = ''
+       return  querry = event.target.value
+        }
+      
+        
     }
     return (
         <div className="container">
             <div className="form-container">
-            <form className="search-box" action="">
-            <label htmlFor="q"> 
-               <input
-              
-               className="search-box__input"
-               id="q"
-               onChange ={ (event) => handleChangle(event)}
-               type="text"
-               placeholder=""
-                
-               />
-               <label htmlFor="" className="placeLabel">Find movie</label>
-               </label>
-            </form>
+                <form className="search-box" action="">
+                    <label htmlFor="q">
+                        <input
+
+                            className="search-box__input"
+                            id="q"
+                           onclick={handleChangle}
+                           // onChange={handleChangle.bind(this)}
+                            type="text"
+                            placeholder=""
+
+                        />
+                        <label htmlFor="" className="placeLabel">Find movie</label>
+                    </label>
+                </form>
             </div>
         </div>
     )
--- src/pages/More.js
+++ src/pages/More.js
@@ -1,27 +1,36 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Search from '../components/search/search'
 import Card from '../components/card/card';
 import Header from '../components/header/header'
- 
-let url = "https://api.themoviedb.org/3/movie/1573?api_key=24cfed25ea68b234c8167f71ba903910"
 
-class More extends Component  {
+let url = ''
+
+class More extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-       //   movieID: 157336 
+            movieID: 13,
+            loading: []
         }
-      }
-      componentDidMount() {
-          this.fetchMovie(url)
-      }
-        fetchMovie = () => {
-           fetch(url)
-           .then( response => response.json())
-           .then(data => {   
-              
-               this.setState({
+    }
+
+
+
+    // `https://api.themoviedb.org/3/search/movie?api_key=24cfed25ea68b234c8167f71ba903910&query=${querry}`
+    componentDidMount() {
+        this.fetchMovie(url)
+    }
+    fetchMovie = (querry) => {
+        console.log(querry)
+        if (querry !== null) {
+            url = `https://api.themoviedb.org/3/search/movie?api_key=24cfed25ea68b234c8167f71ba903910&query=${querry}`
+        }
+        url = `https://api.themoviedb.org/3/movie/${this.state.movieID}?api_key=24cfed25ea68b234c8167f71ba903910`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
                     movieId: data.id,
                     title: data.original_title,
                     moto: data.tagline,
@@ -30,25 +39,51 @@ class More extends Component  {
                     revenue: data.revenue,
                     runtime: data.runtime,
                     poster: data.poster_path,
-                    companies:data.production_companies,
+                    companies: data.production_companies,
                     rating: data.vote_average,
                     type: data.genres,
                     backdrops: data.backdrop_path
-               })
-            
-           })
- 
-       }
-    render() {
-         return (
-        <div className="more">
-            <Header/>
-            <Search/>
-            <Card  data = {this.state}/>
-        </div>
-    )
+                })
+
+            })
+
     }
-   
+    render() {
+        return (
+            <div className="more">
+                <Header />
+                <Search fetchMovie={this.fetchMovie.bind(this)} />
+                <Card data={this.state} />
+            </div>
+        )
+    }
+
+
+    //      suggests = new Bloodhound({
+    //         datumTokenizer: function(datum) {
+    //           return Bloodhound.tokenizers.whitespace(datum.value);
+    //         },
+    //         queryTokenizer: Bloodhound.tokenizers.whitespace,
+    //         remote: {
+    //           url: 'https://api.themoviedb.org/3/search/movie?query=%QUERY&api_key=24cfed25ea68b234c8167f71ba903910',
+    //           filter: function(movies) {
+    //             // Map the remote source JSON array to a JavaScript object array
+    //             return $.map(movies.results, function(movie) {
+    //               return {
+    //                 value: movie.original_title, // search original title
+    //                 id: movie.id // get ID of movie simultaniously
+    //               };
+    //             });
+    //           } // end filter
+    //         } // end remote
+    //       }); // end new Bloodhound
+
+    //    suggests.initialize(); // initialise bloodhound suggestion engine
+
+
+
+
+
 }
 
 export default More
