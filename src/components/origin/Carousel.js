51db6cb7b3770d2685d962985ed68679265a954b
alexkobrin <storyday2012@gmail.com>
2020-11-03 10:53:04 +0100

    work on page origin p2

--- src/components/origin/Carousel.js
+++ src/components/origin/Carousel.js
@@ -1,34 +1,98 @@
 import React, { Component } from "react";
-
+import axios from "axios"
+import './carousel.scss';
 import Slider from "react-slick";
 
 export default class Carousel extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            newRelease: [],
-            Actions: []
+            trending: [],
+            popular: [],
+
+            movieId: ""
+
         }
     }
+
+
     componentDidMount() {
+        axios.get("https:api.themoviedb.org/3/discover/movie?api_key=24cfed25ea68b234c8167f71ba903910&sort_by=popularity.desc&primary_release_year=2020")
+            .then(response => {
+                this.setState({
+                    trending: response.data.results
+
+                })
+                console.log(response.data.results
+                )
+            })
+
+        axios.get("https:api.themoviedb.org/3/movie/popular?api_key=24cfed25ea68b234c8167f71ba903910&language=en-US")
+            .then(response => {
+                this.setState({
+                    popular: response.data.results
+                })
+            })
+
+
+
 
     }
 
+
+
     render() {
+
+
         const settings = {
-            dots: true,
+            dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 3
+            autoPlay: true,
+            centerPadding: true,
+            slidesToShow: 6,
+            slidesToScroll: 3,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                }
+            ]
         };
         return (
             <div>
-                <h2> Multiple items </h2>
+                <h2 className="carousel-suptitle"   >Trending</h2>
                 <Slider {...settings}>
-                    {this.state.newRelease.map( film =>  (
-                        <div className="film">
-                            <img alt=""/>
+                    {this.state.trending.map((film, index) => (
+                        <div className="carousel-item" key="index">
+                            <img src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`} alt="" />
+                            <div className="rating">
+                                <div className="percent">
+                                    <span>{`${film.vote_average * 10}`}</span>
+                                    <span className="percent-icon">%</span>
+                                </div>
+
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
+                <h2 className="carousel-suptitle">Now Popular</h2>
+                <Slider {...settings}>
+
+                    {this.state.popular.map((wer, index) => (
+                        <div className="carousel-item" key="index">
+                            <img src={`https://image.tmdb.org/t/p/w500/${wer.poster_path}`} alt="" />
+                            <div className="rating">
+                                <div className="percent">
+                                    <span>{`${wer.vote_average * 10}`}</span>
+                                    <span className="percent-icon">%</span>
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </Slider>
--- src/components/origin/origin.js
+++ src/components/origin/origin.js
@@ -1,40 +1,36 @@
-import React, { useEffect ,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../header/header';
 import Carousel from './Carousel'
 import './origin.scss';
 
 function Origin() {
 
- const [newReleases , setNewReleases] = useState("");
- useEffect(() => {
-       
-       return () => {
-         
-       }
-   }, [])
- return (
+    const [newReleases, setNewReleases] = useState("");
+    useEffect(() => {
+
+        return () => {
+
+        }
+    }, [])
+    return (
         <section className="origin">
             <div className="container-fluid">
-             <Header/>
-              </div>
+                <Header />
+            </div>
             <div className="container">
                 <div className="colection-title">
-                  <h1> Collections of movies </h1>
-                  <h2>Choose Your favourite film , and learn more about it</h2>
-                  </div>
-
-                  <div className="collection-slider__new">
-                  <div className="collection-slider__title">
-                      New Releases
-                  </div>
-                  <div className="collection-slider__inner">
-                     <Carousel/> 
-                  </div>
-                  </div>
-            </div>
-          
-        
-           </section>
+                    <h1> Collections of movies </h1>
+                    <h3>Choose Your favourite film , click on it and learn more.</h3>
+                </div>
+                </div>
+                <div className="container-fluid">
+                <div className="collection-slider__trending">
+                    <div className="collection-slider__inner">
+                        <Carousel />
+                    </div>
+                </div>
+                </div>
+        </section>
     )
 }
 
