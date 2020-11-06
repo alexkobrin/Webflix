ee3a267b03864c6769edd18d14c8e21b74815324
alexkobrin <storyday2012@gmail.com>
2020-11-06 15:54:55 +0100

    delete folder search

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Home from './pages/home'
 import Auth from './pages/auth'
 import More from './pages/More'
-import Origin from './pages/origin'
+ import Origin from './pages/origin'
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-        <Route exact path="/" component={Home} ></Route>
       <Switch>
+        <Route exact path="/" component={Home} ></Route>
         <Route exact path="/auth" component={Auth} ></Route>
         <Route exact path="/find" component={More} />
         <Route exact path="/origin" component={Origin} />
